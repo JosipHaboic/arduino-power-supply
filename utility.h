230c11d0c4a2eaac16b4f0e526739cc6773e5b7b
jhaboic <josiphaboic@gmail.com>
2021-02-25 22:54:19 +0100

    cleanup

--- utility.h
+++ utility.h
@@ -1,7 +1,7 @@
 uint8_t duty_cycle_to_percent(uint8_t duty_cylce);
 float adc_to_float(uint16_t value);
 float voltage_divider(int r1, int r2);
-String to_json(float vin, float vout, uint8_t duty_cycle);
+
 
 uint8_t duty_cycle_to_percent(uint8_t duty_cycle)
 {
